72bca3d703504a91301381c4a9c2577c9d95c61a
Diligord <johnson.eli.202204@gmail.com>
2024-05-13 22:35:35 -0700

    Update LinguacraftaClient.java
    Modify

--- src/client/java/diligord/linguacrafta/LinguacraftaClient.java
+++ src/client/java/diligord/linguacrafta/LinguacraftaClient.java
@@ -2,15 +2,33 @@ package diligord.linguacrafta;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 
 public class LinguacraftaClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		System.out.println("Hello from init client!");
+        ClientSendMessageEvents.MODIFY_CHAT.register((message) -> {
+			File currentlang = new File("D:/linguacrafta/run/linguacrafta/Valfhod.json");
 
-		ClientSendMessageEvents.MODIFY_CHAT.register((message) -> {
-			return "Hello World!";
+			if (currentlang != null) {
+				System.out.println(currentlang.getName());
+			}
+			return translate(message);
 		});
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+
+	private String translate(String message) {
+		return "ege tiuf ";
 	}
 }
