747533eb45e505d21ae59e1c30f3d4f4642ae4c5
Diligord <johnson.eli.202204@gmail.com>
2024-05-13 17:19:47 -0700

    W skibidi rizz

--- src/client/java/diligord/linguacrafta/LinguacraftaClient.java
+++ src/client/java/diligord/linguacrafta/LinguacraftaClient.java
@@ -1,10 +1,16 @@
 package diligord.linguacrafta;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
 
 public class LinguacraftaClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		System.out.println("Hello from init client!");
+
+		ClientSendMessageEvents.MODIFY_CHAT.register((message) -> {
+			return "Hello World!";
+		});
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 	}
 }
